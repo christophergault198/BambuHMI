5f0517bf7c309280cbf1113ba205dae0e890c8e8
Scooter <talkufy@outlook.com>
2024-04-19 13:25:08 -0400

    Fixed Training Model + Added RIV Integration v1

--- RIV_ML_Testing/Prediction.py
+++ RIV_ML_Testing/Prediction.py
@@ -1,3 +1,5 @@
+import os
+import random
 import tensorflow as tf
 import numpy as np
 
@@ -5,28 +7,37 @@ import numpy as np
 new_model = tf.keras.models.load_model('Build_Plate_Detection.keras')
 
 # Image dimensions
-img_height, img_width = 180, 180 
+img_height, img_width = 224, 224  
 
 # Show the model architecture
 new_model.summary()
 
-# Image URL
-image_url = input("Enter the URL of the image: ")
+# Define the folder containing the images
+folder_path = "toolhead_images"
 
-# Download and load the image
-image_path = tf.keras.utils.get_file('Red_Sunflower', origin=image_url)
-img = tf.keras.utils.load_img(image_path, target_size=(img_height, img_width))
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0)  # Create a batch
+# List all files in the folder
+image_files = os.listdir(folder_path)
 
-# Make predictions
-predictions = new_model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+# Randomly select two images
+random_images = random.sample(image_files, 10)
 
-# Class names
-class_names = ['itemoff', 'itemon']  # Assuming itemoff is class 0 and itemon is class 1
+for image_file in random_images:
+    # Load the image
+    image_path = os.path.join(folder_path, image_file)
+    img = tf.keras.utils.load_img(image_path, target_size=(img_height, img_width))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+    # Make predictions
+    predictions = new_model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+
+    # Class names
+    class_names = ['itemoff', 'itemon']  # itemoff is class 0 and itemon is class 1
+
+    print("Image:", image_file)
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
+    print()
