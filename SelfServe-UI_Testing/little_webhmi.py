246cb827bf3ed689e76655e2b83fa9d9ef2fe6b7
christophergault198 <116657484+christophergault198@users.noreply.github.com>
2024-04-22 23:37:38 -0700

    X1P now has web ui functions

--- SelfServe-UI_Testing/little_webhmi.py
+++ SelfServe-UI_Testing/little_webhmi.py
@@ -1,17 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import sys
 import os
 from send_gcode import send_command
-from dds import publisher
+from dds import publisher, subscribe
+import subprocess
+import json
+import threading
+import time
 
 app = Flask(__name__)
+
+# Global variable to hold the current job name
+current_job_name = "None"
+
+def update_current_job():
+    global current_job_name
+    while True:
+        try:
+            with open('/userdata/Metadata/plate_1.json', 'r') as file:
+                data = json.load(file)
+                # Assuming the structure of the JSON remains consistent
+                name = data['bbox_objects'][0]['name']
+                current_job_name = name
+        except Exception as e:
+            print(f"Error reading or parsing JSON file: {e}")
+        time.sleep(10)
 
 @app.route('/')
 def printer_hmi():
     # Example printer details
     printer_details = {
         'status': 'Printing',
-        'current_job': 'ExampleModel.gcode',
+        'current_job': current_job_name,  # Use the global variable
         'progress': 75,
         'temperature': {
             'nozzle': 215,
@@ -20,22 +40,24 @@ def printer_hmi():
     }
     return render_template('index.html', details=printer_details)
 
-@app.route('/home_printer')
-def home_printer():
-    send_command(publisher, 'G28')  # Assuming 'G28' is the command for homing the printer
-    return 'Printer homing command sent'
+@app.route('/home_xyz_func', methods=['GET','POST'])
+def home_xyz_func():
+    subprocess.run(["/usr/bin/home_xyz.sh"], shell=False)
+    return 'G-code command sent successfully'
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/current_image')
+def current_image():
+    image_path = '/userdata/log/cam/capture/calib_14.jpg'
+    return send_file(image_path, mimetype='image/jpeg')
 
-@app.route('/send_gcode', methods=['POST'])
-def send_gcode():
-    gcode = request.form['gcode']
-    # Handle G-code sending here, e.g., using MQTT or SSH
-    # For example, using the MQTT shell script from the codebase:
-    # subprocess.run(["/path/to/gcode_mqtt_ex.sh", gcode], shell=True)
-    return jsonify({'status': 'success', 'gcode': gcode})
+@app.route('/current_model_image')
+def current_model_image():
+    image_path = '/userdata/log/cam/flc/report/ref_model.png'
+    return send_file(image_path, mimetype='image/png')
+
 
 if __name__ == '__main__':
+    # Start the background thread
+    threading.Thread(target=update_current_job, daemon=True).start()
     app.run(host='0.0.0.0', port=5000)
+
